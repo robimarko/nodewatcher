dc1cbed90d65e3730f4ecc7897a3127427ef1a82
Robert Marko <robimarko@gmail.com>
2017-07-01 12:35:19 +0200

    Add WR841N v13

--- nodewatcher/modules/devices/tplink/wr841nd.py
+++ nodewatcher/modules/devices/tplink/wr841nd.py
@@ -311,6 +311,85 @@ class TPLinkWR841NDv12(TPLinkWR841NDv7):
         }
     }
 
+
+class TPLinkWR841Nv13(cgm_devices.DeviceBase):
+    """
+    TP-Link WR841Nv13 device descriptor.
+    """
+
+    identifier = 'tp-wr841nv13'
+    name = "WR841N (v13)"
+    manufacturer = "TP-Link"
+    url = 'http://www.tp-link.com/'
+    architecture = 'ramips_mt7628'
+    radios = [
+        cgm_devices.IntegratedRadio('wifi0', _("Integrated wireless radio"), [
+            cgm_protocols.IEEE80211BGN(
+                cgm_protocols.IEEE80211BGN.SHORT_GI_20,
+                cgm_protocols.IEEE80211BGN.SHORT_GI_40,
+                cgm_protocols.IEEE80211BGN.RX_STBC1,
+                cgm_protocols.IEEE80211BGN.DSSS_CCK_40,
+            )
+        ], [
+            cgm_devices.AntennaConnector('a1', "Antenna0")
+        ], [
+            cgm_devices.DeviceRadio.MultipleSSID,
+        ])
+    ]
+    switches = [
+        cgm_devices.Switch(
+            'sw0', "Switch0",
+            ports=[0, 1, 2, 3, 4, 6],
+            cpu_port=6,
+            vlans=16,
+            configurable=False,
+            presets=[
+                cgm_devices.SwitchPreset('default', _("Default VLAN configuration"), vlans=[
+                    cgm_devices.SwitchVLANPreset(
+                        'lan0', "Lan0",
+                        vlan=1,
+                        ports=[0, 1, 2, 3, 4, 6],
+                    )
+                ])
+            ]
+        )
+    ]
+    ports = [
+        cgm_devices.EthernetPort('wan0', "Wan0"),
+    ]
+    antennas = [
+        # TODO: This information is probably not correct
+        cgm_devices.InternalAntenna(
+            identifier='a1',
+            polarization='horizontal',
+            angle_horizontal=360,
+            angle_vertical=75,
+            gain=2,
+        )
+    ]
+    port_map = {
+        'openwrt': {
+            'wifi0': 'radio0',
+            'sw0': cgm_devices.SwitchPortMap('switch0', vlans='eth0'),
+            'wan0': 'eth0',
+        }
+    }
+    drivers = {
+        'openwrt': {
+            'wifi0': 'mac80211'
+        }
+    }
+    profiles = {
+        'lede': {
+            'name': 'tl-wr841n-v13',
+            'files': [
+                '*-ramips-mt7628-tl-wr841n-v13-squashfs-factory.bin',
+                '*-ramips-mt7628-tl-wr841n-v13-squashfs-sysupgrade.bin',
+            ]
+        }
+    }
+
+
 # Register the TP-Link WR841ND device.
 cgm_base.register_device('openwrt', TPLinkWR841NDv1)
 cgm_base.register_device('openwrt', TPLinkWR841NDv3)
@@ -321,3 +400,4 @@ cgm_base.register_device('openwrt', TPLinkWR841NDv9)
 cgm_base.register_device('openwrt', TPLinkWR841NDv10)
 cgm_base.register_device('openwrt', TPLinkWR841NDv11)
 cgm_base.register_device('lede', TPLinkWR841NDv12)
+cgm_base.register_device('lede', TPLinkWR841Nv13)
