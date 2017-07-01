606b27522a7741a5bd4ea22eae279eee1b5f7c44
Robert Marko <robimarko@gmail.com>
2017-07-01 12:45:16 +0200

    devices: Add TP Link WR840N v4
    Add TP Link WR840N v4 device descriptor

---
+++ nodewatcher/modules/devices/tplink/wr840n.py
@@ -0,0 +1,84 @@
+from django.utils.translation import ugettext_lazy as _
+
+from nodewatcher.core.generator.cgm import base as cgm_base, protocols as cgm_protocols, devices as cgm_devices
+
+
+class TPLinkWR840Nv4(cgm_devices.DeviceBase):
+    """
+    TP-Link WR840Nv4 device descriptor.
+    """
+
+    identifier = 'tp-wr840nv4'
+    name = "WR840N (v4)"
+    manufacturer = "TP-Link"
+    url = 'http://www.tp-link.com/'
+    architecture = 'ramips_mt7628'
+    radios = [
+        cgm_devices.IntegratedRadio('wifi0', _("Integrated wireless radio"), [
+            cgm_protocols.IEEE80211BGN(
+                cgm_protocols.IEEE80211BGN.SHORT_GI_20,
+                cgm_protocols.IEEE80211BGN.SHORT_GI_40,
+                cgm_protocols.IEEE80211BGN.RX_STBC1,
+                cgm_protocols.IEEE80211BGN.DSSS_CCK_40,
+            )
+        ], [
+            cgm_devices.AntennaConnector('a1', "Antenna0")
+        ], [
+            cgm_devices.DeviceRadio.MultipleSSID,
+        ])
+    ]
+    switches = [
+        cgm_devices.Switch(
+            'sw0', "Switch0",
+            ports=[0, 1, 2, 3, 4, 6],
+            cpu_port=6,
+            vlans=16,
+            configurable=False,
+            presets=[
+                cgm_devices.SwitchPreset('default', _("Default VLAN configuration"), vlans=[
+                    cgm_devices.SwitchVLANPreset(
+                        'lan0', "Lan0",
+                        vlan=1,
+                        ports=[0, 1, 2, 3, 4, 6],
+                    )
+                ])
+            ]
+        )
+    ]
+    ports = [
+        cgm_devices.EthernetPort('wan0', "Wan0"),
+    ]
+    antennas = [
+        # TODO: This information is probably not correct
+        cgm_devices.InternalAntenna(
+            identifier='a1',
+            polarization='horizontal',
+            angle_horizontal=360,
+            angle_vertical=75,
+            gain=2,
+        )
+    ]
+    port_map = {
+        'openwrt': {
+            'wifi0': 'radio0',
+            'sw0': cgm_devices.SwitchPortMap('switch0', vlans='eth0'),
+            'wan0': 'eth0',
+        }
+    }
+    drivers = {
+        'openwrt': {
+            'wifi0': 'mac80211'
+        }
+    }
+    profiles = {
+        'lede': {
+            'name': 'tl-wr840n-v4',
+            'files': [
+                '*-ramips-mt7628-tl-wr840n-v4-squashfs-factory.bin',
+                '*-ramips-mt7628-tl-wr840n-v4-squashfs-sysupgrade.bin',
+            ]
+        }
+    }
+
+# Register the TP-Link WR840N device.
+cgm_base.register_device('lede', TPLinkWR840Nv4)
