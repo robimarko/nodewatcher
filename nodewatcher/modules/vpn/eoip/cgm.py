a2018b4200373db2c8b25a69d4799fa6364b47f3
Robert Marko <robimarko@gmail.com>
2018-10-25 13:39:20 +0200

    Improve interface selection
    This leaves only interface identifier instead of full verbose name.

--- nodewatcher/modules/vpn/eoip/cgm.py
+++ nodewatcher/modules/vpn/eoip/cgm.py
@@ -13,19 +13,20 @@ def eoip_config(node, pkgcfg, cfg):
     Configures EOIP tunnels.
     """
 
-    try:
-        pkgcfg = pkgcfg.get()
-    except models.EOIPTunnelConfig.MultipleObjectsReturned:
-        raise cgm_base.ValidationError(_("Only one EOIP tunnel may be defined."))
+    pkgcfg = pkgcfg.get()
 
     # Check if interface selected.
     if not pkgcfg.interface:
         raise cgm_base.ValidationError(_("EOIP must have a interface defined."))
 
+    eoip_interface = cfg.network.find_named_section('interface', _managed_by=pkgcfg.interface)
+    if not eoip_interface:
+        raise cgm_base.ValidationError(_("Configured EOIP interface not found."))
+
     # Configure EOIP tunnels.
 
     tunnel = cfg['eoip'].add('tunnel')
-    tunnel.interface = pkgcfg.interface
+    tunnel.interface = eoip_interface.get_key()
     tunnel.local_ip = pkgcfg.local_ip
     tunnel.remote_ip = pkgcfg.remote_ip
     tunnel.tunnel_id = pkgcfg.tunnel_id
